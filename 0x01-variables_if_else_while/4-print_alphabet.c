467a6e189d4da81653c0419908f3bcb06b3f16dd
Bazil Onyayo <bazilonyayo@gmail.com>
2022-09-09 16:07:29 -0700

    Created script

---
+++ 0x01-variables_if_else_while/4-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+ 
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		if (letter != 'q' || letter != 'e')
+		{
+			putchar(letter);
+		}
+	}
+	return (0);
+}

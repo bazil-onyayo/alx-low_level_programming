30e5960f51d47b4f37470cb49e58a3b972d6391e
Bazil Onyayo <bazilonyayo@gmail.com>
2022-09-09 12:19:52 -0700

    Updated script

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,10 +2,10 @@
 /**
  * main - Entry point
  *
- * Return: 0
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
+	puts("Programming is like building a multilingual puzzle\n");
 	return (0);
 }

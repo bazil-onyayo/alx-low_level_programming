718f54740d9e7c4f2fce1c3714c84db2e09f4fd3
Bazil Onyayo <bazilonyayo@gmail.com>
2022-09-09 16:12:36 -0700

    Updated script

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -5,12 +5,11 @@
  *
  * Return: Always 0 (Success)
  */
- 
 int main(void)
 {
 	char letter;
 
-	for (letter = 'a'; letter <= 'z'; letter++)
+	for (letter = 'a'; letter <= 'z'; ++letter)
 	{
 		if (letter != 'q' || letter != 'e')
 		{

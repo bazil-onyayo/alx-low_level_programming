42b92668c35512b87c4292ce3318f720a7722f3a
Bazil Onyayo <bazilonyayo@gmail.com>
2022-09-09 12:55:56 -0700

    Created script

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ *main -Entry point
+ *
+ *Return: Always 1 (Failure)
+ */
+int main(void)
+{
+  fprinf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+  return (1);
+}

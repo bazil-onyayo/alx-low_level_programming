d20cf7b42fa2b026ea9b73ab7aa79bc5f4ee3c2c
Bazil Onyayo <bazilonyayo@gmail.com>
2022-09-09 16:19:47 -0700

    Updated script

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -9,12 +9,13 @@ int main(void)
 {
 	char letter;
 
-	for (letter = 'a'; letter <= 'z'; ++letter)
+	for (letter = 'a'; letter <= 'z'; letter++)
 	{
-		if (letter != 'q' || letter != 'e')
+		if (letter != 'q' && letter != 'e')
 		{
 			putchar(letter);
 		}
 	}
+	putchar('\n');
 	return (0);
 }

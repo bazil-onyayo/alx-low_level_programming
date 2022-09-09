d47d212c7cd6c499613194bc986f0f9f5e30b55a
Bazil Onyayo <bazilonyayo@gmail.com>
2022-09-09 13:00:18 -0700

    Updated script

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,6 +7,6 @@
  */
 int main(void)
 {
-	fprinf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	fprintf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 	return (1);
 }

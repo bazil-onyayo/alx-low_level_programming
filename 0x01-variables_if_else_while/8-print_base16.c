4b4a30ac4724a31623acf4b8d0d0605cab63e92b
Bazil Onyayo <bazilonyayo@gmail.com>
2022-09-09 17:22:25 -0700

    Created script

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char n;
+
+	for (n = '0'; n <= '9'; n++)
+	{
+		putchar(n);
+	}
+	for (n = 'a'; n <= 'f'; n++)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+	return (0);
+}

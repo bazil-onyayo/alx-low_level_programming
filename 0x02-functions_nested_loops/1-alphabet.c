5fbd59eb21452a773d9e18ea0026a31966d0d0dc
Bazil Onyayo <bazilonyayo@gmail.com>
2022-09-14 13:23:35 -0700

    Updated script

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always (0) Success
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+		_putchar(c);
+	_putchar('\n');
+	return (0);
+}

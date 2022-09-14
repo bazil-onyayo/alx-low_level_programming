de397eb88b6de14029cd96883843a10c5529ea95
Bazil Onyayo <bazilonyayo@gmail.com>
2022-09-14 14:06:16 -0700

    Created script

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -5,12 +5,13 @@
  *
  * Return: Always (0) Success
  */
-int main(void)
+void print_alphabet(void)
 {
 	char c;
 
 	for (c = 'a'; c <= 'z'; c++)
+	{
 		_putchar(c);
+	}
 	_putchar('\n');
-	return (0);
 }

06fc2e8d778442113f5aeaf309750d445b8a5540
Bazil Onyayo <bazilonyayo@gmail.com>
2022-09-15 03:45:56 -0700

    Added fn that adds two integers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * add - Adds two integers and returns the result
+ * @a: First integer to be added
+ * @b: Second integer to be added
+ *
+ * Return: Sum of a and b
+ */
+int add(int a, int b)
+{
+	int result;
+
+	result = a + b;
+
+	return (result);
+}

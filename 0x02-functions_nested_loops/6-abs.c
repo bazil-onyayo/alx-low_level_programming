1bcae0284c7fddd151239525206609050d4ad8a9
Bazil Onyayo <bazilonyayo@gmail.com>
2022-09-14 21:39:13 -0700

    Created script

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -8,12 +8,12 @@
  */
 int _abs(int)
 {
-	if (int >= 0)
+	if (n >= 0)
 	{
-		_putchar(int);
-	} else if (int < 0)
+		_putchar(n);
+	} else if (n < 0)
 	{
-		_putchar(int * -1);
+		_putchar(n * -1);
 	}
 
 	return (0);

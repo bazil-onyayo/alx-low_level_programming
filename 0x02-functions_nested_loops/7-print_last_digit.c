3680dae779ef339acb920e643cc450434721cc8e
Bazil Onyayo <bazilonyayo@gmail.com>
2022-09-14 22:04:36 -0700

    Added fn that prints last digit of n

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * print_last_digit - Prints the last digit of a number
+ * @n: The number to be checked
+ *
+ * Return: value of the last digit
+ */
+int print_last_digit(int n)
+{
+	return (n % 10);
+}

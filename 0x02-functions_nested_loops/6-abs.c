7dea11d613cba93e89804614c906a3e898193379
Bazil Onyayo <bazilonyayo@gmail.com>
2022-09-14 21:37:42 -0700

    Created script

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _abs - Computes the absolute value of an integer
+ * @arg: The integer to be checked
+ *
+ * Return: Always (0) Success
+ */
+int _abs(int)
+{
+	if (int >= 0)
+	{
+		_putchar(int);
+	} else if (int < 0)
+	{
+		_putchar(int * -1);
+	}
+
+	return (0);
+}

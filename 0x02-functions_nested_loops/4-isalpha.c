513bf1695cb7a71a82bb98b02b19209c88056c4d
Bazil Onyayo <bazilonyayo@gmail.com>
2022-09-14 20:35:18 -0700

    Created script

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _isalpha- Checks for lowercase character
+ * @c: Character to be checked
+ *
+ * Return: 1 if chraracter is a letter, lowercase or uppercase. 0 otherwise
+ */
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z' || c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	return (0);
+}

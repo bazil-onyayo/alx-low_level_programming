f57aadeafd8c59a33ed57fc5cfd272708be5d4cb
Bazil Onyayo <bazilonyayo@gmail.com>
2022-09-14 20:21:13 -0700

    Created script

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * int _islower - Checks for lowercase character
+ * @c: Character to be checked
+ *
+ * Return: 1 if chraracter is lowercase. 0 otherwise
+ */
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}	
+	return (0);
+}

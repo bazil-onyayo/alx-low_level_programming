48a32a95130e27d62a81ec04df9b36361e444434
Bazil Onyayo <bazilonyayo@gmail.com>
2022-09-14 20:39:25 -0700

    Updated script

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -6,7 +6,7 @@
  *
  * Return: 1 if chraracter is a letter, lowercase or uppercase. 0 otherwise
  */
-int _islower(int c)
+int _isalpha(int c)
 {
 	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
 	{

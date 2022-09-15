f95fa6641c8cc2fe10954fcc82f2f1948a54e9e5
Bazil Onyayo <bazilonyayo@gmail.com>
2022-09-14 19:45:26 -0700

    Updated script

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -9,9 +9,9 @@ void print_alphabet_x10(void)
 	char letter;
 	int n;
 
-	for (n = 0; n < 9; n++)
+	for (n = 0; n < 10; n++)
 	{
-		for(letter = 'A'; letter <= 'Z'; letter++) 
+		for(letter = 'a'; letter <= 'z'; letter++) 
 		{
 			_putchar(letter);
 		}

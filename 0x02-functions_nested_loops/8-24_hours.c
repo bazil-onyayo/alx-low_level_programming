a6f345918b19caf6a61270c53509a588a986807b
Bazil Onyayo <bazilonyayo@gmail.com>
2022-09-15 01:13:20 -0700

    Added fn that prints every min starting from 00:00 to 23:59

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,7 +1,8 @@
 #include "main.h"
 
 /**
- * jack_bauer - Prints every minute of the day of Jack Bauer, starting from 00:00 to 23:59.
+ * jack_bauer - Prints every minute of the day of Jack Bauer,
+ *		starting from 00:00 to 23:59.
  */
 void jack_bauer(void)
 {
@@ -13,16 +14,12 @@ void jack_bauer(void)
 		j = 0;
 		while (j < 60)
 		{
-			putchar((i/10) + '0');
-			putchar((i%10) + '0');
-			putchar(':');
-			putchar((j/10) + '0');
-			putchar((j%10) + '0');
-			putchar(('\n');
+			_putchar((i / 10) + '0');
+			_putchar((i % 10) + '0');
+			_putchar(':');
+			_putchar((j / 10) + '0');
+			_putchar((j % 10) + '0');
+			_putchar(('\n');
 		}
 	}
 }
-	
-		
-
-	 

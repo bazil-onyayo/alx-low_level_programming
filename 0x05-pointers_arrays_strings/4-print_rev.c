fd657d49a3dfd1afb1334a36048c7dd0518a85ab
Bazil Onyayo <bazilonyayo@gmail.com>
2022-09-21 11:44:44 -0700

    Added print_rev fucntion

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -10,15 +10,17 @@ void print_rev(char *s)
 	int i;
 	int n;
 
-	for (i = 0; s[i] != '\0'; i++)
+	if (s != '\0')
 	{
-		n = i + 1;
-	}
+		for (i = 0; s[i] != '\0'; i++)
+		{
+			n = i + 1;
+		}
 
-	for (i = n; i > 0; i--)
-	{
-		_putchar(s[i-1]);
-	}
-
+		for (i = n; i > 0; i--)
+		{
+			_putchar(s[i-1]);
+		}
+	}		
 	_putchar('\n');
 }

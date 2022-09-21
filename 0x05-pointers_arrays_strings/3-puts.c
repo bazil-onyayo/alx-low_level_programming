dc3c2b8a4f9dd5fe7c531bb3eec65e2fb2bd31fb
Bazil Onyayo <bazilonyayo@gmail.com>
2022-09-21 09:45:21 -0700

    Added _puts function

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _puts - prints a string,
+ *	followed by a new line, to stdout
+ * @str: a pointer to a string
+ */
+void _puts(char *str)
+{
+	_puts(str);
+	_puts("\n");
+}

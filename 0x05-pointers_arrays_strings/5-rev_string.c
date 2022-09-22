6b1ce56550d8643682fe1665eae37e2b9dc35e91
Bazil Onyayo <bazilonyayo@gmail.com>
2022-09-22 03:41:28 -0700

    Added rev_string function

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,49 @@
+#include "main.h"
+
+/**
+ * rev_string - reverses a string
+ * @s: a pointer to a string
+ */
+void rev_string(char *s)
+{
+	int i;
+	int len;
+	char *str_start;
+	char *str_end;
+	char ch;
+
+	str_start = s;
+	str_end = s;
+
+	if (s[0] != '\0')
+	{
+		for (i = 0; s[i] != '\0'; i++)
+		{
+			len = i + 1;
+		}
+
+		/**
+		 * This for loop results to the end pointer of string s
+		 */
+		for (i = 0; i > (len - 1); i--)
+		{
+			str_end++;
+		}
+
+		/**
+		 * This for loop runs half the length of the string
+		 * to swap all letters except for the middle letter
+		 * for uneven strings
+		 */
+		for (i = 0; i < (len / 2); i++)
+		{
+			ch = *st_end;
+			*st_start = *st_end;
+			*st_end = ch;
+
+			st_start++;
+			st_end--;
+		}
+
+	}
+}

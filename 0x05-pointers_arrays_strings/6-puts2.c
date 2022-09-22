590f2eb6b8e6f7394801a7a2bdaba15b5321d871
Bazil Onyayo <bazilonyayo@gmail.com>
2022-09-22 05:21:17 -0700

    Updated puts2 function

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * _puts - prints a string,
+ *	followed by a new line, to stdout
+ * @str: a pointer to a string
+ */
+void _puts(char *str)
+{
+	int i;
+	int len;
+
+	if (str[0] != '\0')
+	{
+		for (i = 0; str[i] != '\0'; i++)
+		{
+			len = i + 1;
+		}
+
+		for (i = 0; i < (len - 2); i++)
+		{
+			if (i % 2 == 0)
+			{
+				str++;
+				str++;
+				_putchar(str[i]);
+			}
+		}
+		_putchar('\n');
+	}
+}

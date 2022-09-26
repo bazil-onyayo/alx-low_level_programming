d215e5601bd3a2a2ad4f4300d37b4d9fc79b79ea
Bazil Onyayo <bazilonyayo@gmail.com>
2022-09-26 01:30:37 -0700

    Added password generator

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,4 +1,13 @@
-iint main(void)
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - generates random valid passwords for the program 101-crackme
+ *
+ * Return: Always 0
+ */
+int main(void)
 {
         int i;
         int n;

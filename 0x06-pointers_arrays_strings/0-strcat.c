48ec61234a8cf22bec278bfaa2a446002879d66c
Bazil Onyayo <bazilonyayo@gmail.com>
2022-09-26 08:03:34 -0700

    Added _strcat function

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * _strcat - concatenates two strings
+ * @dest: a pointer to the destination string
+ * @src: a pointer to the source string
+ *
+ * Return: a pointer to the resulting string dest
+ */
+char *_strcat(char *dest, char *src)
+{
+	int i;
+	
+	for (i = 0; *dest != '\0'; i++)
+	{
+		dest++;
+	}
+
+	while (*src != '\0')
+	{
+		*dest = *src;
+		*dest++;
+		*src++
+	}
+
+	*dest = '\0';
+
+	return (dest);
+}

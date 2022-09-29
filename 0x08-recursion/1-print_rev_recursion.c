a51941b0a8126d109d1b54e038e66f03fd27b873
Bazil Onyayo <bazilonyayo@gmail.com>
2022-09-29 08:06:02 -0700

    Added _puts_rev_recursion fn

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _print_rev_recusrion - prints a string in reverse
+ * @s: a pointer to a string
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s)
+	{
+		_print_rev_recursions(s + 1);
+		_putchar(*s);
+	}
+}

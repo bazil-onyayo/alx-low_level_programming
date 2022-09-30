d03fc559b9d8f055fa5fa84ab42a1562ec236f9b
Bazil Onyayo <bazilonyayo@gmail.com>
2022-09-29 20:05:31 -0700

    Added factorial function

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -12,7 +12,7 @@ int factorial(int n)
 	{
 		return (-1);
 	}
-	
+
 	if (n == 0)
 	{
 		return (1);
@@ -23,5 +23,5 @@ int factorial(int n)
 		return (1);
 	}
 
-	return n + factorial(n - 1);
+	return (n * factorial(n - 1));
 }
